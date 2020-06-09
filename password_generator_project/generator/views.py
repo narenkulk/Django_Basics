a811a699329dcb818ce4cdfe6833cbd449ada7b0
Narendra KUMAR <narendra.kumar@amadeus.com>
2020-06-09 15:27:37 -0400

    version 2

--- password_generator_project/generator/views.py
+++ password_generator_project/generator/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, "generator/home.html", {'password': 'hwerea898kasdf!@asdlfkj'})
+    return render(request, "generator/home.html")
 
 
-def eggs(request):
-    return HttpResponse("Eggs")
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special_char'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('special_char'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
+    password = ""
+    for x in range(length):
+        password += random.choice(characters)
+    return render(request, "generator/password.html", {"password": password})
+
+
+def about(request):
+    return render(request, "generator/about.html")
--- password_generator_project/password_generator/urls.py
+++ password_generator_project/password_generator/urls.py
@@ -19,6 +19,7 @@ from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('eggs', views.eggs),
+    path('', views.home, name="home"),
+    path('generated_password/', views.password, name="password"),
+    path('about', views.about, name="about"),
 ]
