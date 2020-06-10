96bb86e90f35384d8f92d39f554b4a5b9fde0af1
Narendra KUMAR <narendra.kumar@amadeus.com>
2020-06-10 17:23:53 -0400

    Django_Basics_project3

---
+++ todowoo/todo/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
+
+# Create your views here.
+
+
+def signupuser(request):
+    if request.method == "GET":
+        return render(request, "todo/signupuser.html", {'form': UserCreationForm()})
+    else:
+        # Create new user
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, "todo/signupuser.html", {'form': UserCreationForm(), 'error': "userid already taken"})
+
+        else:
+            return render(request, "todo/signupuser.html", {'form': UserCreationForm(), 'error': "Passwords didn't match"})
+            # Tell the user the passwords didn't match
+
+
+def currenttodos(request):
+    return render(request, "todo/currenttodos.html")
