d3a8ec0e63149a691354adb948c97ed764a04dc9
Narendra KUMAR <narendra.kumar@amadeus.com>
2020-06-11 18:39:14 -0400

    final changes-project3

--- todowoo/todowoo/urls.py
+++ todowoo/todowoo/urls.py
@@ -21,6 +21,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Auth
     path('signup/', views.signupuser, name="signupuser"),
+    path('login/', views.loginuser, name="loginuser"),
+    path('logout/', views.logoutuser, name="logoutuser"),
     # Todos
+    path('', views.home, name="home"),
+    path('create/', views.createtodo, name="createtodo"),
+    path('completed/', views.completedtodos, name="completedtodos"),
     path('current/', views.currenttodos, name="currenttodos"),
+    path('todo/<int:todo_pk>', views.viewtodo, name="viewtodo"),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name="completetodo"),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name="deletetodo"),
 ]
